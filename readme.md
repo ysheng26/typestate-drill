0856618789c430a9117670d77033bcf3bead8aeb
Yun Sheng <uewing@gmail.com>
2026-09-19 11:20:25 -0400

    actor and typesafe together

--- readme.md
+++ readme.md
@@ -11,3 +11,13 @@
 - `&current_state` in `match (&current_state, cmd) {}` is an immutable borrow, however it's dropped after match allowing `current_state = InternalState::Connecting`. This is because of [Non-lexical lifetimes)[https://blog.rust-lang.org/2022/08/05/nll-by-default/]
 - Now `spawn_client` is actually the big switch statement. We'll see how this is fixed
 
+## actor pattern and typestate drill
+- Gemini
+    - **binding modifier**. `(ActiveSession::Ready(mut session), Command::Publish(msg)) => {}` extract the struct from the enum, give me full ownership and bind a local variable called `session` which is mutable
+    - **reference pattern**. `(ActiveSession::Ready(&mut session), Command::Publish(msg)) => {}` extract the struct from the enum, I expect the payload to be a mutable pointer `&mut Session<Ready>`. Follow the pointer and bind `session` to the actual struct.
+    - Use `&` or `&mut` in a pattern when you want to unwrap a pointer.
+    - Use `mut` in a pattern when you already own the data and just want permission to modify your local copy.
+- Me
+    - `&mut` in `&mut session` participates in the pattern matching, where `&mut` is part of the pattern.
+    - `mut` in `mut session` is declaring the new local variable `session` to be `mut`
+
--- src/actor.rs
+++ src/actor.rs
@@ -1,7 +1,7 @@
 use std::sync::mpsc;
 use std::thread;
 
-use crate::session::Disconnected;
+use crate::session::{self, Connecting, Disconnected, Ready, Session};
 
 pub enum Command {
     Connect,
@@ -14,6 +14,12 @@ enum InternalState {
     Disconnected,
     Connecting,
     Ready,
+}
+
+enum ActiveSession {
+    Disconnected(Session<Disconnected>),
+    Connecting(Session<Connecting>),
+    Ready(Session<Ready>),
 }
 
 #[derive(Clone)]
@@ -77,5 +83,46 @@ pub fn spawn_client() -> Client {
         println!("All clients dropped. Actor shutting down");
     });
 
+    Client { sender: tx }
+}
+
+pub fn spawn_client_typestate() -> Client {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let initial_session = Session::new("broker_url");
+        let mut current_state = Some(ActiveSession::Disconnected(initial_session));
+
+        while let Ok(cmd) = rx.recv() {
+            let state = current_state.take().unwrap();
+
+            current_state = Some(match (state, cmd) {
+                (ActiveSession::Disconnected(session), Command::Connect) => {
+                    println!("Connecting to broker...");
+                    let next_session = session.connect();
+                    ActiveSession::Connecting(next_session)
+                }
+                (ActiveSession::Connecting(session), Command::Authenticate) => {
+                    println!("Authenticating...");
+                    let next_session = session.authenticate();
+                    ActiveSession::Ready(next_session)
+                }
+                (ActiveSession::Ready(mut session), Command::Publish(msg)) => {
+                    session.publish(&msg);
+                    ActiveSession::Ready(session)
+                }
+                (ActiveSession::Ready(session), Command::Disconnect) => {
+                    println!("Disconnecting...");
+                    let next_session = session.disconnect();
+                    ActiveSession::Disconnected(next_session)
+                }
+                (original_state, bad_cmd) => {
+                    println!("Invalid command");
+                    original_state
+                }
+            });
+        }
+        println!("All clients dropped. Actor shutting down");
+    });
     Client { sender: tx }
 }
